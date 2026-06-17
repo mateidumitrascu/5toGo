273182eeb2a1c88c4097cc28bd1e5128044a62af
mateidumitrascu <mateidumitrascu32@gmail.com>
2026-06-17 19:16:10 +0300

    chore: graceful server shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,38 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/5fives-to-go/internal/server"
 )
 
 func main() {
 	srv := server.NewHTTPServer()
-	srv.ListenAndServe()
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Error listening and serving: %v\n", err)
+		}
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-signalChan
+
+	ctx, stop := context.WithTimeout(context.Background(), 5*time.Second)
+	defer stop()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Printf("\nServer shutdown with error: %v\n", err)
+	} else {
+		fmt.Println("\nServer shutdown gracefully")
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var (
@@ -16,6 +17,20 @@ func NewMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Hello from %v\n", r)
+		_, err := fmt.Fprintln(w, "Your hello request was received")
+		if err != nil {
+			fmt.Printf("Error responding to client: %+v\n", err)
+		}
+	})
+
+	mux.HandleFunc("GET /shut", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Received request, waiting")
+		time.Sleep(3 * time.Second)
+
+		_, err := fmt.Fprintln(w, "Shutdown test request processed")
+		if err != nil {
+			fmt.Printf("Error responding to client: %+v\n", err)
+		}
 	})
 
 	return mux
@@ -23,7 +38,10 @@ func NewMux() *http.ServeMux {
 
 func NewHTTPServer() *http.Server {
 	return &http.Server{
-		Addr:    fullAddress,
-		Handler: NewMux(),
+		Addr:         fullAddress,
+		Handler:      NewMux(),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
 }
